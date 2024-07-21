0f185481d73ce4a4b023413237992b9024a03ea7
incognitotgt <tgt@incognitotgt.me>
2024-07-21 13:07:48 -0400

    new config format+workflow

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,10 +2,11 @@ import landing from "@/app/_images/landing.gif"
 import Image from "next/image";
 export default function HomePage() {
   return (
-    <main className="flex h-full flex-col items-center text-center">
-      <div className="text-2xl font-semibold mt-15">Stardust, the container streaming platform</div>
-      <div className="mt-5 text-muted-foreground">Stardust is a container streaming platform that allows you to stream your containers to any device, anywhere.</div>
+    <main className="flex h-full flex-col items-center text-center gap-4 mt-15">
+      <div className="text-2xl font-semibold">Stardust, the container streaming platform</div>
+      <div className="text-muted-foreground">Stardust is a container streaming platform that allows you to stream your containers to any device, anywhere.</div>
       <Image src={landing} alt="Stardust" className="justify-self-center w-[80rem] h-[50rem] rounded-lg p-4" />
+
     </main>
   );
 }
