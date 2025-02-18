134a5b9432eb86ccb8e1a2f85b436cc64aad6c46
tgt <tgt@incognitotgt.me>
2025-02-17 23:08:05 -0500

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./global.css";
+import { Analytics } from "@vercel/analytics/next";
 import { RootProvider } from "fumadocs-ui/provider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -23,6 +24,7 @@ export default function Layout({ children }: { children: ReactNode }) {
 		<html lang="en" className={inter.className} suppressHydrationWarning>
 			<body>
 				<RootProvider>{children}</RootProvider>
+				<Analytics />
 			</body>
 		</html>
 	);
